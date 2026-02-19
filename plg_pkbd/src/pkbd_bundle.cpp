0d4639e3dd1e6a07d3f884031e971c3e77c333e9
TheTechnobear <mharris@technobear.com>
2026-02-19 09:57:50 +0100

    Fix pico strip when sliding downwards

--- plg_pkbd/src/pkbd_bundle.cpp
+++ plg_pkbd/src/pkbd_bundle.cpp
@@ -233,7 +233,7 @@ namespace
                 break;
 
             case 2:
-                if(std::abs(long(s-last_))<200 && s>threshold_)
+                if(std::abs(long(s)-last_)<200 && s>threshold_)
                 {
                     int o = origin_;
                     o -= s;
